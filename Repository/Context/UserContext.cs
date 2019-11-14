217c346b7604816fd9154fef5df32dfe1bcf6868
pnp1995 <pritampriyanand@gmail.com>
2019-11-14 11:22:47 +0530

    [Add. Admin and Collaborator is created]

--- Repository/Context/UserContext.cs
+++ Repository/Context/UserContext.cs
@@ -3,21 +3,59 @@ using Common.CollaboratorModel;
 using Common.LabelModel;
 using Common.Models;
 using Common.NotesModel;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 
 namespace FundooRepository.Context
 {
     public class UserContext: DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         { }
+        /// <summary>
+        /// Gets or sets the user detail.
+        /// </summary>
+        /// <value>
+        /// The user detail.
+        /// </value>
         public DbSet<UserModel> UserDetail { get; set; }
-        public DbSet<NotesModel> NotesDetail { get; set; }       
+        /// <summary>
+        /// Gets or sets the notes detail.
+        /// </summary>
+        /// <value>
+        /// The notes detail.
+        /// </value>
+        public DbSet<NotesModel> NotesDetail { get; set; }
+        /// <summary>
+        /// Gets or sets the label detail.
+        /// </summary>
+        /// <value>
+        /// The label detail.
+        /// </value>
         public DbSet<LabelModel> LabelDetail { get; set; }
+        /// <summary>
+        /// Gets or sets the collaborator detail.
+        /// </summary>
+        /// <value>
+        /// The collaborator detail.
+        /// </value>
         public DbSet<CollaboratorModel> CollaboratorDetail { get; set; }
+        /// <summary>
+        /// Gets or sets the admin detail.
+        /// </summary>
+        /// <value>
+        /// The admin detail.
+        /// </value>
         public DbSet<AdminModel> AdminDetail { get; set; }
+        /// <summary>
+        /// Gets or sets the adminlogin detail.
+        /// </summary>
+        /// <value>
+        /// The adminlogin detail.
+        /// </value>
         public DbSet<AdminLoginModel> AdminloginDetail { get; set; }
     }
 }  
